66d28ec25532d89d7e0614cfa72b48fe4acf2ed7
Nic Fisher <n.f@live.com.au>
2019-03-28 17:03:46 +1100

    add actionsheet

--- app/profile/screens/profile-page.js
+++ app/profile/screens/profile-page.js
@@ -1,5 +1,6 @@
 import * as auth from '../../auth/actions/auth.action';
 import React, {Component} from "react";
+import ActionSheet from 'react-native-actionsheet';
 import {View, Text, TouchableOpacity, StyleSheet, Image} from "react-native";
 import {connect} from 'react-redux';
 import ImagePicker from "react-native-image-picker";
@@ -21,6 +22,11 @@ class ProfilePage extends Component {
       avatarSource: null
     };
   }
+
+  showActionSheet = () => {
+    this.ActionSheet.show();
+  };
+
   uploadPhoto = () => {
     ImagePicker.launchImageLibrary(photoOptions, response => {
       console.log('Response = ', response);
@@ -43,16 +49,52 @@ class ProfilePage extends Component {
     )
   };
 
+  takePhoto = () => {
+    ImagePicker.launchCamera(photoOptions, response => {
+      console.log('Response = ', response);
+      if (response.uri == null || response.uri === undefined) return;
+      const { uri } = response;
+      console.log('URI: ', uri);
+      this.setState({avatarSource: {uri: uri}});
+
+      const fileName = `${uuidv4()}.${fileExtension(uri)}`;
+      const contentType = mime.lookup(uri) || "application/octet-stream";
+
+      console.log('fileName: ', fileName)
+      console.log('contentType: ', contentType)
+
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+    
+    });
+  };
+
+  modalOptions = [
+    { title: 'Upload Photo', action: this.uploadPhoto },
+    { title: 'Take A Photo', action: this.takePhoto },
+    { title: 'Cancel' }
+  ];
+
   render() {
     const {avatarSource} = this.state;
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this.uploadPhoto}>
-          {!!avatarSource ? <Image source={avatarSource} style={{height: 120, width: 120, borderRadius: 60, marginBottom: 50}} /> : <View style={{backgroundColor: '#FFA7C4', height: 120, width: 120, borderRadius: 60, marginBottom: 50}} />}
+        <TouchableOpacity onPress={this.showActionSheet}>
+          {avatarSource ? <Image source={avatarSource} style={{height: 120, width: 120, borderRadius: 60, marginBottom: 50}} /> : <View style={{backgroundColor: '#FFA7C4', height: 120, width: 120, borderRadius: 60, marginBottom: 50}} />}
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.props.logout()}>
           <Text>Logout</Text>
         </TouchableOpacity>
+        <ActionSheet
+          ref={o => (this.ActionSheet = o)}
+          options={this.modalOptions.map(option => option.title)}
+          cancelButtonIndex={2}
+          onPress={index => {
+            const action = this.modalOptions[index].action;
+            action ? action() : null;
+          }}
+        />
       </View>
     )
   }
